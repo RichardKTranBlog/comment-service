2fba5aaeb662aff1a9cc83f3c8dab082f3ea3ea2
richardktran <richardktran.dev@gmail.com>
2023-07-18 01:58:40 +0700

    change

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -7,12 +7,12 @@ class CommentController extends Controller
     public function index()
     {
         return ([
-            'comment-1' => 'This is comment 1',
-            'comment-2' => 'This is comment 2',
-            'comment-3' => 'This is comment 3',
-            'comment-4' => 'This is comment 4',
-            'comment-5' => 'This is comment 5',
-            'comment-6' => 'This is comment 6',
+            'comment-1' => 'Branch rb-1 This is comment 1',
+            'comment-2' => 'Branch rb-1 This is comment 2',
+            'comment-3' => 'Branch rb-1 This is comment 3',
+            'comment-4' => 'Branch rb-1 This is comment 4',
+            'comment-5' => 'Branch rb-1 This is comment 5',
+            'comment-6' => 'Branch rb-1 This is comment 6',
         ]);
     }
 }
